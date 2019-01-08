fa701e4b954007dc7ff8e1f38775f8e81b04b8f8
Sunny Jhun Ibabao <sunnyjhun.ibabao@gmail.com>
2019-01-08 15:53:21 +0800

    includes to_do list

--- app/controllers/connect.php
+++ app/controllers/connect.php
@@ -1,9 +1,9 @@
 <?php 
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'todo_app_db';
+$host = 'db4free.net';
+$username = 'sunny_9999';
+$password = 'Sunz.1996';
+$dbname = 'todo_list1996';
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
